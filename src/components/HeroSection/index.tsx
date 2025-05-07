021640ad33343656270770b6cdae616f81d9aabe
KostyaFirstov <k.firstov2014@gmail.com>
2025-05-08 00:30:51 +0300

    new projects for deploy

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,110 +1,113 @@
-import React from 'react'
-import styles from './HeroSection.module.css'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+import React from 'react';
+import styles from './HeroSection.module.css';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 interface HeroSectionProps {
-	main?: 'main' | 'second' | 'third'
-	title: string
-	video: string
+  main?: 'main' | 'second' | 'third';
+  title: string;
+  video: string;
+  isHeroImg?: boolean;
 }
 
 const HeroSection: React.FC<HeroSectionProps> = ({
-	main = 'main',
-	title,
-	video
+  main = 'main',
+  title,
+  video,
+  isHeroImg,
 }) => {
-	const blockRef = React.useRef<HTMLDivElement>(null)
-	const qBlocks = gsap.utils.selector(blockRef)
+  const blockRef = React.useRef<HTMLDivElement>(null);
+  const qBlocks = gsap.utils.selector(blockRef);
 
-	React.useEffect(() => {
-		const block = blockRef.current
-		let resTop =
-			main === 'main' ? '24.58vw' : main === 'second' ? '19.2vw' : '17.2vw'
+  React.useEffect(() => {
+    const block = blockRef.current;
+    let resTop =
+      main === 'main' ? '24.58vw' : main === 'second' ? '19.2vw' : '17.2vw';
 
-		let mm = gsap.matchMedia()
+    let mm = gsap.matchMedia();
 
-		mm.add('(max-width: 768px)', () => {
-			resTop =
-				main === 'main' ? '32.58vw' : main === 'second' ? '27.2vw' : '25.2vw'
-		})
+    mm.add('(max-width: 768px)', () => {
+      resTop =
+        main === 'main' ? '32.58vw' : main === 'second' ? '27.2vw' : '25.2vw';
+    });
 
-		mm.add('(min-width: 768px)', () => {
-			let tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: block,
-					start: 'top top',
-					scrub: 1,
-					pin: true,
-					pinSpacing: true,
-					invalidateOnRefresh: true
-				}
-			})
+    mm.add('(min-width: 768px)', () => {
+      let tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: block,
+          start: 'top top',
+          scrub: 1,
+          pin: true,
+          pinSpacing: true,
+          invalidateOnRefresh: true,
+        },
+      });
 
-			tl.to(qBlocks('h1'), {
-				opacity: 0,
-				y: -100,
-				duration: 1.6,
-				ease: 'expo.inOut'
-			})
+      tl.to(qBlocks('h1'), {
+        opacity: 0,
+        y: -100,
+        duration: 1.6,
+        ease: 'expo.inOut',
+      });
 
-			tl.fromTo(
-				qBlocks('.hero__image'),
-				{ top: resTop },
-				{
-					top: 0,
-					duration: 4
-				},
-				'-0.05'
-			)
+      tl.fromTo(
+        qBlocks('.hero__image'),
+        { top: resTop },
+        {
+          top: 0,
+          duration: 4,
+        },
+        '-0.05',
+      );
 
-			tl.fromTo(
-				qBlocks('.hero__image video'),
-				{ width: '92vw' },
-				{
-					width: '100vw',
-					borderRadius: 0,
-					duration: 4
-				},
-				'-0.05'
-			)
-		})
-	}, [])
+      tl.fromTo(
+        qBlocks(`.hero__image ${isHeroImg ? 'img' : 'video'}`),
+        { width: '92vw' },
+        {
+          width: '100vw',
+          borderRadius: 0,
+          duration: 4,
+        },
+        '-0.05',
+      );
+    });
+  }, []);
 
-	return (
-		<>
-			<div ref={blockRef} className={styles.root}>
-				<div className='hero__wrapper'>
-					<div className={styles.title__wrapper}>
-						<h1
-							className={
-								main === 'main'
-									? styles.title
-									: main === 'second'
-									? styles.title_small
-									: styles.title_smaller
-							}
-						>
-							{title}
-						</h1>
-					</div>
-					<div className='hero__image'>
-						<video
-							className={styles.image}
-							preload='meta'
-							muted={true}
-							loop={true}
-							autoPlay={true}
-						>
-							<source src={video} type='video/mp4' />
-						</video>
-					</div>
-				</div>
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <div ref={blockRef} className={styles.root}>
+        <div className="hero__wrapper">
+          <div className={styles.title__wrapper}>
+            <h1
+              className={
+                main === 'main'
+                  ? styles.title
+                  : main === 'second'
+                  ? styles.title_small
+                  : styles.title_smaller
+              }
+            >
+              {title}
+            </h1>
+          </div>
+          <div className="hero__image">
+            {isHeroImg && <img src={video} alt={title} />}
+            <video
+              className={styles.image}
+              preload="meta"
+              muted={true}
+              loop={true}
+              autoPlay={true}
+            >
+              <source src={video} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
--- src/pages/ProjectPage/index.tsx
+++ src/pages/ProjectPage/index.tsx
@@ -1,61 +1,68 @@
-import React from 'react'
-import styles from './ProjectPage.module.css'
-import HeroSection from '../../components/HeroSection'
-import TextBlock from '../../components/TextBlock'
-import ImageBlock from '../../components/ImageBlock'
-import ListBlock, { IList } from '../../components/ListBlock'
-import NextProject from './NextProject'
-import ProjectPhones from './ProjectPhones'
-import ProjectSlides from './ProjectSlides'
-import Transition from '../../transition'
-import useLocoScroll from '../../hooks/useLocoScroll'
-import usePageTitle from '../../hooks/usePageTitle'
+import React from 'react';
+import styles from './ProjectPage.module.css';
+import HeroSection from '../../components/HeroSection';
+import TextBlock from '../../components/TextBlock';
+import ImageBlock from '../../components/ImageBlock';
+import ListBlock, { IList } from '../../components/ListBlock';
+import NextProject from './NextProject';
+import ProjectPhones from './ProjectPhones';
+import ProjectSlides from './ProjectSlides';
+import Transition from '../../transition';
+import useLocoScroll from '../../hooks/useLocoScroll';
+import usePageTitle from '../../hooks/usePageTitle';
 
 interface IProjectPage {
-	id: string
-	name: string
-	desc: string
-	about: React.ReactNode
-	reels: string
-	link: string
-	details: IList
-	imageBlock: string
-	slidesImages: string[]
-	phonesImages: string[]
+  id: string;
+  name: string;
+  desc: string;
+  about: React.ReactNode;
+  reels: string;
+  link: string;
+  details: IList;
+  imageBlock: string;
+  slidesImages: string[];
+  phonesImages: string[];
+  isHeroImg?: boolean;
 }
 
 const ProjectPage: React.FC<IProjectPage> = ({
-	id,
-	name,
-	desc,
-	about,
-	details,
-	link,
-	reels,
-	imageBlock,
-	phonesImages,
-	slidesImages
+  id,
+  name,
+  desc,
+  about,
+  details,
+  link,
+  reels,
+  imageBlock,
+  phonesImages,
+  slidesImages,
+  isHeroImg,
 }) => {
-	usePageTitle(name)
-	useLocoScroll()
+  usePageTitle(name);
+  useLocoScroll();
 
-	return (
-		<Transition>
-			<div className={styles.root}>
-				<HeroSection title={name} main='third' video={reels} />
-				<TextBlock
-					text={desc}
-					title={about}
-					links={[{ path: link, name: 'Смотреть сайт', isNext: true }]}
-				/>
-				<ImageBlock src={imageBlock} alt={name} />
-				<ListBlock desc='Детали проекта' list={details} />
-				<ProjectSlides images={slidesImages} />
-				<ProjectPhones images={phonesImages} />
-				<NextProject id={+id} />
-			</div>
-		</Transition>
-	)
-}
+  return (
+    <Transition>
+      <div className={styles.root}>
+        <HeroSection
+          title={name}
+          main="third"
+          video={reels}
+          isHeroImg={isHeroImg}
+        />
+        <TextBlock
+          text={desc}
+          title={about}
+          links={[{ path: link, name: 'Смотреть сайт', isNext: true }]}
+        />
+        <ImageBlock src={imageBlock} alt={name} />
+        <ListBlock desc="Детали проекта" list={details} />
+        <ProjectSlides images={slidesImages} />
+        <ProjectPhones images={phonesImages} />
+        <NextProject id={+id} />
+      </div>
+    </Transition>
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
